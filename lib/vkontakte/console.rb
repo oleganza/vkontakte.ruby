9847fae86659076a82af67a863dee047f32f9e97
Oleg Andreev <oleganza@gmail.com>
2008-08-08 21:03:50 +0400

    fixed namespace in console

--- lib/vkontakte/console.rb
+++ lib/vkontakte/console.rb
@@ -1,7 +1,7 @@
 require 'irb'
 module Vkontakte
   module Console
-    
+    include Vkontakte
     def initialize_console
       puts "Vkontakte v#{Vkontakte::VERSION} (help! for more info)"
     end
@@ -12,12 +12,12 @@ module Vkontakte
   
 Usage example:
 
-  me = Vkontakte::User.new "invizko@gmail.com", "whatever"
+  me = User.new "invizko@gmail.com", "whatever"
   me.id #=> 5567476 
   me.videos #=> [{"duration"=>"4:17", "title"=>"Gabriel", "performer"=>"Lamb", "operate"=>"40134438,1522,5567476,'2ca4745378',257"}, {"duration"=>"1:14", "title"=>"Generation of Terror", "performer"=>"Fexamot", "operate"=>"39117745,1527,5567476,'7b61c70c8a',74"}...]
 
   me.friends.each do |f|
-    friend = VkontateRuby::User.new(f) #created by-id
+    friend = User.new(f) #created by-id
     if friend.friends.any? {|ff| ff == me.id }
       p "\#{friend.name} is your mutual friend"
     else
